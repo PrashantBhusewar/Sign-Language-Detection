3c3f707c4cccd3a83f8529579dad8db94c76147b
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-03 20:26:23 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sign_language",
+    version="0.0.0",
+    author="Prashant",
+    author_email="prashantbhusewar1599@gmail.com",
+    packages=find_packages()
+)
